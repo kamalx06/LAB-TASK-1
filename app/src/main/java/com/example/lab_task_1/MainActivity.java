88d0f875295eb96b6aea03f154ab27e1441ce972
kamalx06 <kamalxkhalilov@gmail.com>
2024-09-22 21:48:21 +0300

    Added button with text change functionalities and run this app with AVD

--- app/src/main/java/com/example/lab_task_1/MainActivity.java
+++ app/src/main/java/com/example/lab_task_1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab_task_1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void Button1changetext(View view) {
+        TextView changetext = (TextView) findViewById(R.id.text);
+        changetext.setText("Android Studio");
     }
 }
